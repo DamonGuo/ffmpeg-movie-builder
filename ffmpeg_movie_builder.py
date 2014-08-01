71fb979ea373ba0010215f6b0e06eeda01700d9e
Ryohei Ueda <garaemon@gmail.com>
2014-08-01 21:34:29 +0900

    update

--- ffmpeg_movie_builder.py
+++ ffmpeg_movie_builder.py
@@ -2,9 +2,38 @@
 import subprocess
 import os, sys
 import tempfile
+
+def getcolor(colorname):
+    colors = {
+        'clear': '\033[0m',
+        'black': '\033[30m',
+        'red': '\033[31m',
+        'green': '\033[32m',
+        'yellow': '\033[33m',
+        'blue': '\033[34m',
+        'purple': '\033[35m',
+        'cyan': '\033[36m',
+        'white': '\033[37m'
+        }
+    def func2(c):
+        return colors[colorname] + c + colors['clear']
+
+    return func2
+
+black  = getcolor('black')
+red    = getcolor('red')
+green  = getcolor('green')
+yellow = getcolor('yellow')
+blue   = getcolor('blue')
+purple = getcolor('purple')
+cyan   = getcolor('cyan')
+white  = getcolor('white')
+
 class Task():
   def run(self):
     pass
+  def exec_command(self, commands):
+    subprocess.check_call(commands)
 
 class ImageGenerateTask(Task):
   def __init__(self, text, 
@@ -30,8 +59,23 @@ class ImageGenerateTask(Task):
                "-gravity", "center",
                "-draw", "text 0, 0 '%s'" % (self.text),
                self.output]
-    subprocess.check_call(command)
+    print green("generating image from text: %s -> %s" %(self.text, self.output))
+    self.exec_command(command)
 
+class MovieConvertTask(Task):
+  def __init__(self, input):
+    self.input = input
+    self.output = tempfile.mktemp(".mp4")
+  def run(self):
+    command = ["ffmpeg", "-loglevel", "quiet",
+               "-i", self.input,
+               "-q:a", "1",
+               "-q:v", "1",
+               "-vf", "scale=trunc(iw/2)*2:trunc(ih/2)*2",
+               self.output]
+    print green("converting movie to mp4: %s -> %s" % (self.input, self.output))
+    self.exec_command(command)
+    
 class MovieGenerateTask(Task):
   def __init__(self, input, duration = 4.0):
     self.output_prefix = tempfile.mktemp()
@@ -48,13 +92,18 @@ class MovieGenerateTask(Task):
     command1 = ["convert", self.input,
                 "-quality", "0",
                 self.mid_image1]
-    command2 = ["ffmpeg", "-r", str(1.0 / self.duration),
+    command2 = ["ffmpeg",
+                "-loglevel", "quiet",
+                "-r", str(1.0 / self.duration),
                 "-i", self.output_prefix + "_%d.png",
                 self.output]
-    subprocess.check_call(command0)
-    subprocess.check_call(command1)
-    subprocess.check_call(command2)
-               
+    print green("copying image: %s -> %s" % (self.input, self.mid_image0))
+    self.exec_command(command0)
+    print green("copying image: %s -> %s" % (self.input, self.mid_image1))
+    self.exec_command(command1)
+    print green("generating movie from two images: (%s, %s) -> %s"
+                % (self.mid_image0, self.mid_image1, self.output))
+    self.exec_command(command2)
     
 def usage():
   print "ffmpeg_builder.py [-m|c|i] arg0 [-m|c|i] arg1 ... output"
@@ -65,26 +114,24 @@ def main():
   movie_files = []
   image_generate_tasks = []               #string -> image
   movie_generate_tasks = []               #image -> movie
+  movie_convert_tasks = []
   while True:
     if len(argv) == 1:
       # output
-      print "writing to ", argv[0]
       output = argv[0]
       break
     else:
       option = argv[0]
       option_arg = argv[1]
       if option == "-m":
-        print "use ", option_arg, " as movie"
-        movie_files.append(option_arg)
+        movie_convert_tasks.append(MovieConvertTask(option_arg))
+        movie_files.append(movie_convert_tasks[-1].output)
       elif option == "-c":
-        print "generate caption from string", option_arg
         image_generate_tasks.append(ImageGenerateTask("hello world"))
         movie_generate_tasks.append(MovieGenerateTask(
           image_generate_tasks[-1].output))
         movie_files.append(movie_generate_tasks[-1].output)
       elif option == "-i":
-        print "use the image as caption: ", option_arg
         movie_generate_tasks.append(MovieGenerateTask(option_arg))
         movie_files.append(movie_generate_tasks[-1].output)
       argv = argv[2:]
@@ -92,8 +139,10 @@ def main():
     i.run()
   for i in movie_generate_tasks:
     i.run()
+  for i in movie_convert_tasks:
+    i.run()
   command = ["mmcat"] + movie_files + [output]
   print " ".join(command)
-  subprocess.check_call(command)
+  # subprocess.check_call(command)
 if __name__ == "__main__":
   main()
